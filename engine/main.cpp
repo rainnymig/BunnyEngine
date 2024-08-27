61964b29e7fd5b87c98c509376f93c6bed4e77ec
rainnymig <lego_cyp@163.com>
2024-08-27 18:03:25 +0200

    basic window with GLFW

--- engine/main.cpp
+++ engine/main.cpp
@@ -2,11 +2,43 @@
 
 #include <iostream>
 #include <memory>
+#include <GLFW/glfw3.h>
 
-int main() {
+int main(void)
+{
     std::cout << "Welcome to Bunny Engine!\n";
 
-    std::unique_ptr<Bunny::Render::Renderer> renderer = std::make_unique<Bunny::Render::VulkanRenderer>();
-    renderer->render();
-    
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Bunny Engine", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    // glClearColor( 0.4f, 0.3f, 0.4f, 0.0f );
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        // glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
