9ef16865365471d99a06809944b13e900f873c31
rainnymig <lego_cyp@163.com>
2024-10-15 23:19:04 +0200

    add basic mass point

---
+++ lib/physics/headers/MassPoint.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Vector3.h"
+
+namespace Bunny::Physics
+{
+class MassPoint
+{
+  public:
+    //  update the particle's kinematics state
+    void update(float duration);
+
+  private:
+    //  get amount of damping applied to linear motion
+    //  this is to reduce numeric instability
+    constexpr static float getLinearMotionDamping();
+
+    Vector3 mPosition;
+    Vector3 mVelocity;
+    Vector3 mAcceleration;
+
+    //  holds inverse mass because it's easier to model infinite mass
+    float mInverseMass;
+};
+} // namespace Bunny::Physics
---
+++ lib/physics/src/MassPoint.cpp
@@ -0,0 +1,28 @@
+#include "MassPoint.h"
+
+#include <cassert>
+
+void Bunny::Physics::MassPoint::update(float duration)
+{
+    //  no need to update if mass is infinite
+    if (mInverseMass == 0)
+    {
+        return;
+    }
+
+    assert(duration > 0);
+
+    mPosition.addScaled(mVelocity, duration);
+
+    Vector3 resultingAcceleration = mAcceleration;
+
+    mVelocity.addScaled(resultingAcceleration, duration);
+
+    //  add damping to reduce numeric instability
+    mVelocity = mVelocity * std::pow(getLinearMotionDamping(), duration);
+}
+
+constexpr float Bunny::Physics::MassPoint::getLinearMotionDamping()
+{
+    return 0.999f;
+}
