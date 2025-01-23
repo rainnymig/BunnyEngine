985cce1ecd32bbb5bba5d349bf89910cb4a8df3c
rainnymig <lego_cyp@163.com>
2025-01-23 21:00:52 +0100

    add fastgltf, WIP2

--- lib/rendering/vulkan/headers/Vertex.h
+++ lib/rendering/vulkan/headers/Vertex.h
@@ -58,4 +58,39 @@ struct BasicVertex
         return attributeDescriptions;
     }
 };
+
+struct NormalVertex
+{
+    glm::vec3 mPosition;
+    glm::vec3 mNormal;
+    glm::vec4 mColor;
+    glm::vec2 mTexCoord;
+
+    static constexpr std::array<VkVertexInputAttributeDescription, 4> getAttributeDescriptions()
+    {
+        std::array<VkVertexInputAttributeDescription, 4> attributeDescriptions{};
+
+        attributeDescriptions[0].binding = 0;
+        attributeDescriptions[0].location = 0;
+        attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[0].offset = offsetof(NormalVertex, mPosition);
+
+        attributeDescriptions[1].binding = 0;
+        attributeDescriptions[1].location = 1;
+        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[1].offset = offsetof(NormalVertex, mNormal);
+
+        attributeDescriptions[2].binding = 0;
+        attributeDescriptions[2].location = 2;
+        attributeDescriptions[2].format = VK_FORMAT_R32G32B32A32_SFLOAT;
+        attributeDescriptions[2].offset = offsetof(NormalVertex, mColor);
+
+        attributeDescriptions[3].binding = 0;
+        attributeDescriptions[3].location = 3;
+        attributeDescriptions[3].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[3].offset = offsetof(NormalVertex, mTexCoord);
+
+        return attributeDescriptions;
+    }
+};
 } // namespace Bunny::Render
--- lib/rendering/vulkan/src/Renderable.cpp
+++ lib/rendering/vulkan/src/Renderable.cpp
@@ -1,12 +1,131 @@
 #include "Renderable.h"
 
 #include "Mesh.h"
+#include "Vertex.h"
 
 #include <glm/mat4x4.hpp>
 #include <glm/ext/matrix_transform.hpp>
 
+#include <fastgltf/glm_element_traits.hpp>
+#include <fastgltf/core.hpp>
+#include <fastgltf/tools.hpp>
+#include <fastgltf/util.hpp>
+
 namespace Bunny::Render
 {
+Scene::Scene(MeshAssetsBank* bank)
+    : mMeshAssetsBank(bank)
+{
+}
+
+bool Scene::loadFromGltfFile(std::string_view filePath)
+{
+    std::filesystem::path path(filePath);
+    constexpr auto gltfOptions = fastgltf::Options::DontRequireValidAssetMember | fastgltf::Options::AllowDouble | fastgltf::Options::LoadGLBBuffers | fastgltf::Options::LoadExternalBuffers;
+    // fastgltf::Options::LoadExternalImages;
+    auto loadedFile = fastgltf::GltfDataBuffer::FromPath(filePath);
+    if (loadedFile.error() != fastgltf::Error::None)
+    {
+        return false;
+    }
+    fastgltf::Parser parser;
+    auto parseResult = parser.loadGltf(loadedFile.get(), path.parent_path(), gltfOptions);
+    if (parseResult.error() != fastgltf::Error::None)
+    {
+        return false;
+    }
+    fastgltf::Asset& gltf = parseResult.get();
+
+    std::vector<uint32_t> indices;
+    std::vector<NormalVertex> vertices;
+
+    for (fastgltf::Mesh& mesh : gltf.meshes)
+    {
+        Mesh newMesh;
+        newMesh.mName = mesh.name;
+
+        indices.clear();
+        vertices.clear();
+
+        //  create mesh surfaces
+        for (auto&& primitive : mesh.primitives)
+        {
+            Surface newSurface;
+            newSurface.mStartIndex = indices.size();
+
+            size_t initialVtx = vertices.size();
+
+            //  load indices
+            {
+                fastgltf::Accessor& indexAccessor = gltf.accessors[primitive.indicesAccessor.value()];
+                newSurface.mCount = indexAccessor.count;
+
+                indices.reserve(indices.size() + indexAccessor.count);
+
+                fastgltf::iterateAccessor<uint32_t>(gltf, indexAccessor, [&](uint32_t idx){
+                    indices.push_back(idx);
+                });
+            }
+
+            //  load vertex positions
+            {
+                fastgltf::Accessor& posAccessor = gltf.accessors[primitive.findAttribute("POSITION")->accessorIndex];
+                vertices.resize(vertices.size() + posAccessor.count);
+
+                fastgltf::iterateAccessorWithIndex<glm::vec3>(gltf, posAccessor,
+                [&vertices, initialVtx](glm::vec3 vec, size_t idx){
+                    NormalVertex newVertex;
+                    newVertex.mPosition = vec;
+                    newVertex.mNormal = {1, 0, 0};
+                    newVertex.mColor = {0.8f, 0.8f, 0.8f, 1.0f};
+                    newVertex.mTexCoord = {0, 0};
+                    vertices[initialVtx + idx] = newVertex;
+                });
+            }
+
+            //  load vertex normal
+            auto normalAttr = primitive.findAttribute("NORMAL");
+            if (normalAttr != primitive.attributes.end())
+            {
+                fastgltf::Accessor& normalAccessor = gltf.accessors[normalAttr->accessorIndex];
+                fastgltf::iterateAccessorWithIndex<glm::vec3>(gltf, normalAccessor,
+                [&vertices, initialVtx](glm::vec3 norm, size_t idx) {
+                    vertices[initialVtx + idx].mNormal = norm;
+                });
+            }
+
+            //  load vertex color
+            auto colorAttr = primitive.findAttribute("COLOR_0");
+            if (colorAttr != primitive.attributes.end())
+            {
+                fastgltf::Accessor& colorAccessor = gltf.accessors[colorAttr->accessorIndex];
+                fastgltf::iterateAccessorWithIndex<glm::vec4>(gltf, colorAccessor,
+                [&vertices, initialVtx](glm::vec4 col, size_t idx) {
+                    vertices[initialVtx + idx].mColor = col;
+                });
+            }
+
+            //  load vertex texcoord
+            auto TexCoordAttr = primitive.findAttribute("TEXCOORD_0");
+            if (TexCoordAttr != primitive.attributes.end())
+            {
+                fastgltf::Accessor& texCoordAccessor = gltf.accessors[TexCoordAttr->accessorIndex];
+                fastgltf::iterateAccessorWithIndex<glm::vec2>(gltf, texCoordAccessor,
+                [&vertices, initialVtx](glm::vec2 coord, size_t idx) {
+                    vertices[initialVtx + idx].mTexCoord = coord;
+                });
+            }
+        
+            //  load material
+            //  TBD
+
+            newMesh.mSurfaces.push_back(newSurface);
+        }
+    }
+
+    return true;
+}
+
 void Scene::render(VkCommandBuffer commandBuffer)
 {
 }
