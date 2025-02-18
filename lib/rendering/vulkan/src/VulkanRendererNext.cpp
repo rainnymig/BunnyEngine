d9fa97f423eb4fc1a7b0396c2eaf950b8bac0e74
Yupei Cai <yupei.cai@ubisoft.com>
2025-02-18 16:49:38 +0100

    kinda fix camera view coordinate system

--- lib/rendering/vulkan/src/VulkanRendererNext.cpp
+++ lib/rendering/vulkan/src/VulkanRendererNext.cpp
@@ -11,8 +11,6 @@
 
 #include <vk_mem_alloc.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <stb_image.h>
