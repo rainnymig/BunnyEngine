5fe123ba2bc5b2ee8014ab794b010bd70523d184
rainnymig <lego_cyp@163.com>
2025-02-20 23:48:22 +0100

    add engine next basic draft

--- playground/main.cpp
+++ playground/main.cpp
@@ -1,25 +1,43 @@
 #include "Timer.h"
 
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
 #include <fmt/core.h>
-#include <thread>
 #include <chrono>
+#include <vector>
+#include <string>
+
+struct TestData
+{
+    int m1;
+    float m2;
+    std::string m3;
+    glm::mat4 mat1;
+    glm::vec4 v1;
+};
 
 int main()
 {
     using namespace std::chrono_literals;
 
     Bunny::Utils::BasicTimer timer;
+
+    const size_t num = 1000000;
+
+    std::vector<TestData> v1(num);
+    std::vector<TestData> v2;
+    std::vector<TestData> v3;
+
     timer.start();
-
-    fmt::print("time now is {}, delta time is {}\n", timer.getTime(), timer.getDeltaTime());
-
-    std::this_thread::sleep_for(16ms);
+    v2.reserve(num);
+    std::copy(v1.begin(), v1.end(), std::back_inserter(v2));
     timer.tick();
-    fmt::print("time now is {}, delta time is {}\n", timer.getTime(), timer.getDeltaTime());
-
-    std::this_thread::sleep_for(10ms);
+    fmt::print("delta time is {}\n", timer.getDeltaTime());
     timer.tick();
-    fmt::print("time now is {}, delta time is {}\n", timer.getTime(), timer.getDeltaTime());
+    v3.reserve(num);
+    memcpy(v3.data(), v1.data(), num * sizeof(TestData));
+    timer.tick();
+    fmt::print("delta time is {}\n", timer.getDeltaTime());
 
     return 0;
 }
