a9d3a72c8d297e44bd4039528699380591fdbe87
Yupei Cai <yupei.cai@ubisoft.com>
2025-03-07 17:56:36 +0100

    WIP

---
+++ engine-next/src/World.cpp
@@ -0,0 +1,45 @@
+#include "World.h"
+
+#include "VulkanRenderResources.h"
+
+#include <fastgltf/glm_element_traits.hpp>
+#include <fastgltf/core.hpp>
+#include <fastgltf/tools.hpp>
+#include <fastgltf/util.hpp>
+
+#include <filesystem>
+
+namespace Bunny::Engine
+{
+BunnyResult WorldLoader::loadGltfToWorld(
+    std::string_view filePath, World outWorld)
+{
+    std::filesystem::path path(filePath);
+    constexpr auto gltfOptions = fastgltf::Options::DontRequireValidAssetMember | fastgltf::Options::AllowDouble |
+                                 fastgltf::Options::LoadGLBBuffers | fastgltf::Options::LoadExternalBuffers;
+    // fastgltf::Options::LoadExternalImages;
+    auto loadedFile = fastgltf::GltfDataBuffer::FromPath(path);
+    if (loadedFile.error() != fastgltf::Error::None)
+    {
+        return BUNNY_SAD;
+    }
+    fastgltf::Parser parser;
+    auto parseResult = parser.loadGltf(loadedFile.get(), path.parent_path(), gltfOptions);
+    if (parseResult.error() != fastgltf::Error::None)
+    {
+        return BUNNY_SAD;
+    }
+    fastgltf::Asset& gltf = parseResult.get();
+
+    //  load materials
+
+    //  load textures
+
+    //  load meshes
+
+    //  load world structure
+
+    return BUNNY_HAPPY;
+}
+
+} // namespace Bunny::Engine
