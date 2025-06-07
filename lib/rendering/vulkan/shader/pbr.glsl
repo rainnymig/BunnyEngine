47397b1af599da6a44c4e86058406afa6d8fd05d
rainnymig <lego_cyp@163.com>
2025-06-07 15:48:53 +0200

    pbr shader lighting calculation

--- lib/rendering/vulkan/shader/pbr.glsl
+++ lib/rendering/vulkan/shader/pbr.glsl
@@ -9,8 +9,11 @@
 struct Light 
 {
     vec3 dirOrPos;
-    float intensity;    //  temp name, will have different meaning for point and directional light
+    float intensity;    //  dir light - illuminance (lux or lumen/m2); point/spot light - luminous power (lumen)
     vec3 color;
+    float inverseInfluenceRadius;
+    float innerAngle;
+    float outerAngle;   
     uint type;
 };
 
@@ -24,6 +27,7 @@ layout(set = 0, binding = 0) uniform LightData
 layout(set = 0, binding = 1) uniform CameraData
 {
     vec3 position;
+    float exposure;
 } cameraData;
 
 //  parameters: https://google.github.io/filament/Filament.html#materialsystem/parameterization
@@ -51,25 +55,25 @@ layout(set = 2, binding = 1) uniform sampler2D textures[];
 
 //  normal distribution function (specular D)
 //  ndoth: normal dot halfway vector
-float d_ggx(float ndoth, float roughness) {
-    float a = ndoth * roughness;
-    float k = roughness / (1.0 - ndoth * ndoth + a * a);
+float d_ggx(float ndoth, float alpha) {
+    float a = ndoth * alpha;
+    float k = alpha / (1.0 - ndoth * ndoth + a * a);
     return k * k * (1.0 / PI);
 }
 
 //  visibility term (specular V)
-float v_smithGgxCorrelated(float ndotv, float ndotl, float roughness)
+float v_smithGgxCorrelated(float ndotv, float ndotl, float alpha)
 {
-    float a2 = roughness * roughness;
+    float a2 = alpha * alpha;
     float ggxV = ndotl * sqrt(ndotv * ndotv * (1.0 - a2) + a2);
     float ggxL = ndotv * sqrt(ndotl * ndotl * (1.0 - a2) + a2);
     return 0.5 / (ggxV + ggxL);
 }
 
 //  visibility term fast (specular V)
-float v_smithGgxCorrelatedFast(float ndotv, float ndotl, float roughness)
+float v_smithGgxCorrelatedFast(float ndotv, float ndotl, float alpha)
 {
-    float a = roughness;
+    float a = alpha;
     float ggxV = ndotl * (ndotv * (1.0 - a) + a);
     float ggxL = ndotv * (ndotl * (1.0 - a) + a);
     return 0.5 / (ggxV + ggxL);
@@ -88,6 +92,44 @@ vec3 f_schlick(float u, vec3 f0)
 float fd_lambert()
 {
     return 1.0 / PI;
+}
+
+//  BRDF
+vec3 brdf(vec3 baseColor, vec3 normal, vec3 lightDir, vec3 viewDir, float metallic, float roughness, float reflectance)
+{
+    vec3 halfVec = normalize(lightDir + viewDir);
+    float ndotv = abs(dot(normal, viewDir)) + 1e-5;     //  adding a small value to prevent it being 0 at grazing view angle
+    float ndotl = clamp(dot(normal, lightDir), 0.0, 1.0);
+    float ndoth = clamp(dot(normal, halfVec), 0.0, 1.0);
+    float ldoth = clamp(dot(lightDir, halfVec), 0.0, 1.0);
+
+    //  map from perceptual roughness to actual roughness
+    float alpha = roughness * roughness;
+    vec3 f0 = 0.16 * reflectance * reflectance * (1.0 - metallic) + baseColor * metallic;
+    vec3 diffuseColor = (1.0 - metallic) * baseColor;
+
+    float D = d_ggx(ndoth, alpha);
+    vec3 F = f_schlick(ldoth, f0);
+    float V = v_smithGgxCorrelated(ndotv, ndotl, alpha);
+
+    //  specular component
+    vec3 Fr = D * V * F;
+
+    //  Fr multi scattering compensation
+    //  ?
+
+    //  diffuse component
+    vec3 Fd = diffuseColor * fd_lambert();
+
+    return Fr + Fd;
+}
+
+float getSquareFalloffAttenuation(vec3 posToLight, float invRadius)
+{
+    float distanceSquare = dot(posToLight, posToLight);
+    float factor = distanceSquare * invRadius * invRadius;
+    float smoothFactor = max(1.0 - factor * factor, 0.0);
+    return (smoothFactor * smoothFactor) / max(distanceSquare, 1e-4);
 }
 
 //  mostly based on the implementation in Filament: https://google.github.io/filament/Filament.html
@@ -126,27 +168,24 @@ vec3 calculateLighting(PbrMaterial material, Light light, vec3 fragPos, vec3 vie
     }
 
     vec3 lightDir = light.type == DIRECTIONAL ? -light.dirOrPos : normalize(light.dirOrPos - fragPos);
-    vec3 halfVec = normalize(lightDir + viewDir);
 
-    float ndotv = abs(dot(normal, viewDir)) + 1e-5;     //  adding a small value to prevent it being 0 at grazing view angle
+    vec3 f = brdf(baseColor.rgb, normal, lightDir, viewDir, metallic, roughness, reflectance);
+
+    //  calculate luminance
     float ndotl = clamp(dot(normal, lightDir), 0.0, 1.0);
-    float ndoth = clamp(dot(normal, halfVec), 0.0, 1.0);
-    float ldoth = clamp(dot(lightDir, halfVec), 0.0, 1.0);
+    vec3 outLuminance;
+    if (light.type == DIRECTIONAL)
+    {
+        float illuminance = light.intensity * ndotl;
+        outLuminance = f * illuminance * light.color;
+    }
+    else if (light.type == POINT)
+    {
+        vec3 posToLight =  light.dirOrPos - fragPos;
+        float attenuation = getSquareFalloffAttenuation(posToLight, light.inverseInfluenceRadius);
+        outLuminance = f * light.intensity * attenuation * ndotl * light.color;
+    }
 
-    float D = d_ggx(ndoth, roughness);
-
-    vec3 f0 = 0.16 * reflectance * reflectance * (1.0 - metallic) + baseColor * metallic;
-    float F = f_schlick(ldoth, f0);
-
-    float V = v_smithGgxCorrelated(ndotv, ndotl, roughness);
-
-    //  specular component
-    vec3 Fr = D * V * F;
-
-    //  diffuse component
-    vec3 diffuseColor = (1.0 - metallic) * baseColor.rgb;
-    vec3 Fd = diffuseColor * fd_lambert();
-
-    vec3 result = material.baseColor.xyz;
+    vec3 result = outLuminance * cameraData.exposure;
     return result;
 }
